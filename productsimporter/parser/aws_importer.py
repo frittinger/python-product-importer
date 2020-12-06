a3582fde005c572b5a8e76fd6cfed235988c8fe2
Frank Rittinger <frank.rittinger@virtual-identity.com>
2020-12-06 20:43:37 +0100

    added reading xml file from AWS

---
+++ productsimporter/parser/aws_importer.py
@@ -0,0 +1,26 @@
+import boto3
+from productsimporter.parser import lparser
+
+s3client = boto3.client(
+    's3',
+    region_name='us-east-1'
+)
+
+# These define the bucket and object to read
+bucketname = 'net-schnegg-import'
+file_to_read = 'simple.xml'
+
+# Create a file object using the bucket and object key.
+fileobj = s3client.get_object(
+    Bucket=bucketname,
+    Key=file_to_read
+)
+# open the file object and read it into the variable filedata.
+filedata = fileobj['Body'].read()
+
+# file data will be a binary stream.  We have to decode it
+contents = filedata.decode('utf-8')
+#
+# # Once decoded, you can treat the file as plain text if appropriate
+# print(contents)
+lparser.parse_xml(filedata)
